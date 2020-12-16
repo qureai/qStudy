790399b388f6eab631af7cf209604644bb263991
kevalnagda <knagda008@gmail.com>
2020-12-16 14:04:53 +0530

    Update desktop environment

--- slicer/entrypoint.sh
+++ slicer/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DISPLAY=:1
-Xvfb $DISPLAY -screen 0 1024x768x16 &
-fluxbox -screen 0 &
-x11vnc -display $DISPLAY -bg -forever -usepw -xkb &
+Xvfb $DISPLAY -screen 0 1600x900x24 &
+sleep 5; nohup startxfce4 &
+x11vnc -display $DISPLAY -forever -usepw -xkb &
 wait
