1d2afa1121262e6a4c478322af0cda72267c67dc
kevalnagda <knagda008@gmail.com>
2021-02-02 20:13:17 +0530

    Fix

--- portal/client/src/pages/AppLogin/services.js
+++ portal/client/src/pages/AppLogin/services.js
@@ -2,7 +2,10 @@ import serviceConnect from '../../libs/axios-connect';
 
 const loginService = (loginData) => {
   return serviceConnect.post('/api/session', loginData)
-    .then(({ data }) => data);
+    .then(({ data }) => data)
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 export default loginService;
