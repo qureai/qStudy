67e69cb89bf8100023d12d9e8eeb2feaa1b767d7
kevalnagda <knagda008@gmail.com>
2021-02-03 12:40:03 +0530

    Update

--- portal/server/src/server.js
+++ portal/server/src/server.js
@@ -26,6 +26,22 @@ let corsOptions = {
 
 const app = express();
 app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+    );
+    res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,Cache-Control,Accept,X-Access-Token ,X-Requested-With, Content-Type, Access-Control-Request-Method"
+    );
+    if (req.method === "OPTIONS") {
+    return res.status(200).end();
+    }
+    next();
+});
 const MongoStore = connectStore(session);
 
 // Obscure backend
