c8dcac0d45c3e73c95eadb5e62b24becf893f9b6
Sasank Chilamkurthy <sasankchilamkurthy@gmail.com>
2021-02-04 14:17:57 +0530

    some fixes

--- slicer/entrypoint.sh
+++ slicer/entrypoint.sh
@@ -4,5 +4,5 @@ Xvfb $DISPLAY -screen 0 1500x800x24 &
 sleep 5; nohup startxfce4 &
 x11vnc -display $DISPLAY -bg -forever -usepw -xkb &
 Slicer-4.11.0-2020-09-12-linux-amd64/Slicer &
-python3 slicer_api.py
+python slicer_api.py
 wait
