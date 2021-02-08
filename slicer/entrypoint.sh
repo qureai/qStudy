6e08805ea67765ae14ef1146836574cb98c4d7ec
keval <knagda008@gmail.com>
2021-02-08 11:05:59 +0530

    Fix x11vnc crash

--- slicer/entrypoint.sh
+++ slicer/entrypoint.sh
@@ -2,7 +2,7 @@
 export DISPLAY=:1
 Xvfb $DISPLAY -screen 0 1500x800x24 &
 sleep 5; nohup startxfce4 &
-x11vnc -display $DISPLAY -bg -forever -usepw -xkb &
+x11vnc -display $DISPLAY -loop -bg -forever -usepw -xkb -reopen &
 Slicer-4.11.0-2020-09-12-linux-amd64/Slicer &
 python slicer_api.py
 wait
