1231dbe806a197c868eefd121637f73128992a1e
Ubuntu <ubuntu@ip-172-31-0-92.ap-south-1.compute.internal>
2021-02-09 15:45:03 +0000

    Update x11vnc config

--- slicer/entrypoint.sh
+++ slicer/entrypoint.sh
@@ -2,7 +2,7 @@
 export DISPLAY=:1
 Xvfb $DISPLAY -screen 0 1500x800x24 &
 sleep 5; nohup startxfce4 &
-x11vnc -display $DISPLAY -loop -bg -forever -usepw -xkb -reopen &
+x11vnc -display $DISPLAY -loop -bg -forever -usepw -xkb -reopen -noxrecord -noxfixes -noxdamage -ncache 10 &
 Slicer-4.11.0-2020-09-12-linux-amd64/Slicer &
 python slicer_api.py
 wait
