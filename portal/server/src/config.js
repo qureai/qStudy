0bf1497f72d966bc1f2b7f786445f35bcf4fbda4
Ubuntu <ubuntu@ip-172-31-0-92.ap-south-1.compute.internal>
2021-02-11 09:04:24 +0000

    Add credentials to .env

--- portal/server/src/config.js
+++ portal/server/src/config.js
@@ -1,9 +1,9 @@
 export const {
     PORT = 5001,
     NODE_ENV = 'development',
-    MONGO_URI = 'mongodb://qStudy_root:bigcamp93@mongodb:27017/qStudy_db?authSource=admin',
+    MONGO_URI = 'mongodb://username:password@mongodb:27017/qStudy_db?authSource=admin', // add your username and password
     SESS_NAME = 'qStudy',
-    SESS_SECRET = 'qStudy!session', //keep random string in env
+    SESS_SECRET = 'qStudy!session', // keep random string in env
     SESS_LIFETIME = 1000 * 60 * 60 * 24
 } = process.env;
 
--- slicer/entrypoint.sh
+++ slicer/entrypoint.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 export DISPLAY=:1
 Xvfb $DISPLAY -screen 0 1500x800x24 &
-sleep 5; nohup startxfce4 &
+sleep 5
+
+nohup startxfce4 &
+
+mkdir -p /root/.vnc
+x11vnc -storepasswd $VNC_PASSWORD /root/.vnc/passwd &
 x11vnc -display $DISPLAY -loop -bg -forever -usepw -xkb -reopen -noxrecord -noxfixes -noxdamage -ncache 10 &
+sleep 5
+
 Slicer-4.11.0-2020-09-12-linux-amd64/Slicer &
+ln -s ~/Slicer-4.11.0-2020-09-12-linux-amd64/Slicer ~/Desktop/Slicer
+
 python slicer_api.py
 wait
