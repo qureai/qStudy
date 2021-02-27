4688e826dd127d96d2b75b533f0413dcd6d88b62
Ubuntu <ubuntu@ip-172-31-0-92.ap-south-1.compute.internal>
2021-02-27 11:40:12 +0000

    Update

--- slicer/entrypoint.sh
+++ slicer/entrypoint.sh
@@ -7,7 +7,7 @@ nohup startxfce4 &
 
 mkdir -p /root/.vnc
 x11vnc -storepasswd $VNC_PASSWORD /root/.vnc/passwd &
-x11vnc -display $DISPLAY -loop -bg -forever -usepw -xkb -reopen -noxrecord -noxfixes -noxdamage -ncache 10 &
+x11vnc -display $DISPLAY -loop -bg -forever -usepw -xkb -repeat -reopen -noxrecord -ncache 10 -q &
 sleep 5
 
 Slicer-4.11.0-2020-09-12-linux-amd64/Slicer &
